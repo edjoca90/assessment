c044570b162d7ea56624289d45b99f6c396295c4
JohanCardenas <c_ecardenas@tgi.com.co>
2025-05-30 10:17:40 -0500

    start proyect SB

---
+++ src/main/java/com/johanCardenas/tps_test/TpsTestApplication.java
@@ -0,0 +1,13 @@
+package com.johanCardenas.tps_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johanCardenas/tps_test/TpsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johanCardenas.tps_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
